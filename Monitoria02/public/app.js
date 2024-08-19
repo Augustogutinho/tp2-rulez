abaf292ee97745aa04f5edba9d572f9eb4d92f38
Augusto <143622825+Augustogutinho@users.noreply.github.com>
2024-08-19 11:31:02 -0300

    Update app.js

--- Monitoria02/public/app.js
+++ Monitoria02/public/app.js
@@ -1,11 +1,10 @@
-// public/scripts.js
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
     form.addEventListener('submit', (event) => {
       const nameInput = document.getElementById('name');
       if (nameInput.value.trim() === '') {
         alert('O nome não pode estar vazio.');
-        event.preventDefault(); // Impede o envio do formulário
+        event.preventDefault();
       }
     });
   });
