b8b5145aaa278fd3338e10e03c6b98ee6c9c7cf7
Capulus-Canis <143612912+Capulus-Canis@users.noreply.github.com>
2024-09-09 20:32:19 -0300

    Update servidor.js

--- serv_localhost/servidor.js
+++ serv_localhost/servidor.js
@@ -8,6 +8,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static("public"));
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://DaviJorge:Garrison2020@cluster.eckz9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Conectado com Sucesso ao MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 let fileData = JSON.parse(fs.readFileSync(`users.json`));
 
 app.get("/", (requisicao, resposta) => {
