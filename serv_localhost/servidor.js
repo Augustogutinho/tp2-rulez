456d15774ab534044698e781bd3df541aa9c370c
Augusto <augusto.souza@gsuite.iff.edu.br>
2024-10-22 11:59:30 -0300

    atraso

--- serv_localhost/servidor.js
+++ serv_localhost/servidor.js
@@ -1,4 +1,4 @@
-const port = 8080;
+const port = 3000;
 const express = require("express");
 const fs = require("fs");
 
@@ -7,6 +7,33 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static("public"));
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://DaviJorge:Garrison2020@cluster.eckz9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 let fileData = JSON.parse(fs.readFileSync(`users.json`));
 
